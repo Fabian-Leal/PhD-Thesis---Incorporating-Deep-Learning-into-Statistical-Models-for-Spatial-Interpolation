6438db417d4732438ec7bbaab6301036aa7f59df
Fabian Leal <fabianlealv@gmail.com>
2024-04-24 17:48:25 +1000

    rearranging_and_commenting

--- Paper 3 PhD BNNs for multivariate predictions/DK_Group_Shapley_VS/dk_model.py
+++ Paper 3 PhD BNNs for multivariate predictions/DK_Group_Shapley_VS/dk_model.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from sklearn.model_selection import KFold
+from sklearn.model_selection import KFold, train_test_split
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import numpy as np
 import matplotlib.pyplot as plt
@@ -47,7 +47,6 @@ class DeepKrigingTrainer:
         test_losses = []   # To store test losses during training
 
         # Training loop
-#        for step in range(601):
         for step in range(601):
             pre = model(x_train)
             mse = criterion(pre, y_train)
@@ -74,17 +73,56 @@ class DeepKrigingTrainer:
             plt.grid(True)
             plt.show()
 
-    def train_neural_network(self):
+    def train_neural_network(self, mode="cross", test_size=None):
         torch.manual_seed(42)
         np.random.seed(42)
 
-        # Define the number of folds for cross-validation
-        num_folds = 10
-        kf = KFold(n_splits=num_folds, shuffle=True, random_state=42)
+        if mode == "cross":
+            # Define the number of folds for cross-validation
+            num_folds = 10
+            kf = KFold(n_splits=num_folds, shuffle=True, random_state=42)
 
-        # Perform k-fold cross-validation
-        for fold, (train_index, test_index) in enumerate(kf.split(self.deposit_data)):
-            train_data, test_data = self.deposit_data.iloc[train_index], self.deposit_data.iloc[test_index]
+            # Perform k-fold cross-validation
+            for fold, (train_index, test_index) in enumerate(kf.split(self.deposit_data)):
+                train_data, test_data = self.deposit_data.iloc[train_index], self.deposit_data.iloc[test_index]
+
+                if self.covariates is not None:
+                    x_train = torch.tensor(train_data[self.phi_columns + self.covariates].values, dtype=torch.float32)
+                    x_test = torch.tensor(test_data[self.phi_columns + self.covariates].values, dtype=torch.float32)
+                else:
+                    x_train = torch.tensor(train_data[self.phi_columns].values, dtype=torch.float32)
+                    x_test = torch.tensor(test_data[self.phi_columns].values, dtype=torch.float32)
+
+                y_train = torch.tensor(train_data['Density_gcm3'].values.reshape(-1, 1), dtype=torch.float32)
+                y_test = torch.tensor(test_data['Density_gcm3'].values.reshape(-1, 1), dtype=torch.float32)
+
+                model = DeepKriging(self.p)
+                criterion = nn.MSELoss()
+                optimizer = optim.Adam(model.parameters(), lr=0.005)
+
+                self.train_model(model, optimizer, criterion, x_train, y_train, x_test, y_test)
+
+                # Store metrics for this fold
+                self.test_predictions_fold = model(x_test).detach().numpy().flatten()
+                self.test_mse_list.append(mean_squared_error(y_test, self.test_predictions_fold))
+                self.test_mae_list.append(mean_absolute_error(y_test, self.test_predictions_fold))
+
+                y_test = test_data['Density_gcm3'].values
+
+                # Calculate adjusted R-squared
+                n = len(y_test)
+                mean_y_test = np.mean(y_test)
+                sst = np.mean((y_test - mean_y_test) ** 2)
+                ssr = np.mean((self.test_predictions_fold - y_test) ** 2)
+                r2 = 1 - (ssr / sst)
+                adjusted_r2 = 1 - ((1 - r2) * (n - 1) / (n - self.p - 1))
+                self.test_adjusted_r2_list.append(adjusted_r2)
+
+        elif mode == "regular":
+            if test_size is None:
+                raise ValueError("For regular mode, test_size parameter must be specified.")
+
+            train_data, test_data = train_test_split(self.deposit_data, test_size=test_size, random_state=42)
 
             if self.covariates is not None:
                 x_train = torch.tensor(train_data[self.phi_columns + self.covariates].values, dtype=torch.float32)
@@ -103,10 +141,9 @@ class DeepKrigingTrainer:
             self.train_model(model, optimizer, criterion, x_train, y_train, x_test, y_test)
 
             # Store metrics for this fold
-            test_predictions_fold = model(x_test).detach().numpy().flatten()
-            self.test_mse_list.append(mean_squared_error(y_test, test_predictions_fold))
-            self.test_mae_list.append(mean_absolute_error(y_test, test_predictions_fold))
-
+            self.test_predictions = model(x_test).detach().numpy().flatten()
+            self.test_mse_list.append(mean_squared_error(y_test, self.test_predictions))
+            self.test_mae_list.append(mean_absolute_error(y_test, self.test_predictions))
 
             y_test = test_data['Density_gcm3'].values
 
@@ -114,15 +151,13 @@ class DeepKrigingTrainer:
             n = len(y_test)
             mean_y_test = np.mean(y_test)
             sst = np.mean((y_test - mean_y_test) ** 2)
-            ssr = np.mean((test_predictions_fold - y_test) ** 2)
+            ssr = np.mean((self.test_predictions - y_test) ** 2)
             r2 = 1 - (ssr / sst)
             adjusted_r2 = 1 - ((1 - r2) * (n - 1) / (n - self.p - 1))
-
             self.test_adjusted_r2_list.append(adjusted_r2)
-        
-            # Print metrics for the current fold
-            #print(f"\nMetrics for Fold {fold + 1}:")
-            #self.print_metrics(y_test.numpy(), test_predictions_fold, "Test", self.p)
+
+        else:
+            raise ValueError("Invalid mode. Choose either 'cross' or 'regular'.")
 
         # Print average metrics across folds
         print("\nAverage Metrics Across Folds:")
